8c29f32b6463ff2ad5db4ad62292a556593697d5
Hoon-Hub <gwi01304@naver.com>
2023-03-08 22:18:45 +0900

    Login Form added
    **로그인 관련 정보 추가**
    1. 로그인 기본 유효성 검사 추가
    2. axios 관련 내용 추가 필요
    3. zustand-store-loginauth 설정 추가
    4. /index.tsx 경로 route 추가
    5. useNavigate 이용하여 로그인 성공시, state 상태를 추가, main 페이지로 이동

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from 'pages/auth/login'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
