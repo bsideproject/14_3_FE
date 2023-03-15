58457432fa5eb62c1e22add243c4b35b203b8509
JE <wpdbsx@naver.com>
2023-03-15 11:55:13 +0900

    type: feat persist 예제 추가 1. zustand persist 예제 추가

--- src/store/modules/TestRegister.ts
+++ src/store/modules/TestRegister.ts
@@ -1,4 +1,5 @@
-import { create } from "zustand";
+import { create, StateCreator } from "zustand";
+import { persist, PersistOptions } from "zustand/middleware";
 
 type REGISTER_INFO = {
   email: string;
@@ -11,18 +12,37 @@ type TEST_STATE = {
   registerInfo: Array<REGISTER_INFO>;
   insertId: (registerInfo: REGISTER_INFO) => void;
 };
-const testRegisterStore = create<TEST_STATE>((set) => ({
-  registerInfo: [
+type PillListState = {
+  registerInfo: Array<REGISTER_INFO>;
+  insertId: (registerInfo: REGISTER_INFO) => void;
+};
+
+type pillListPersist = (
+  config: StateCreator<PillListState>,
+  options: PersistOptions<PillListState>
+) => StateCreator<PillListState>;
+
+const testRegisterStore = create<TEST_STATE>(
+  (persist as pillListPersist)(
+    (set) => ({
+      registerInfo: [
+        {
+          email: "test@gmail.com",
+          nickName: "테스터",
+          password: "12341234",
+          gender: true,
+          birthDt: "2000-02-02",
+        },
+      ],
+      insertId: (registerInfo: REGISTER_INFO): void =>
+        set((state) => ({
+          registerInfo: state.registerInfo.concat(registerInfo),
+        })),
+    }),
     {
-      email: "test@gmail.com",
-      nickName: "테스터",
-      password: "12341234",
-      gender: true,
-      birthDt: "2000-02-02",
-    },
-  ],
-  insertId: (registerInfo: REGISTER_INFO): void =>
-    set((state) => ({ registerInfo: state.registerInfo.concat(registerInfo) })),
-}));
+      name: "testRegister",
+    }
+  )
+);
 
 export default testRegisterStore;
