20b6f9fca7539bb83a475b2a6920604baec32446
DoubleD <53948297+Hoon-Hub@users.noreply.github.com>
2023-03-30 18:11:56 +0900

    Shkim/login (#9)
    * login logic added
    
    1. 로그인 관련 로직을 추가하였습니다. (수정)
    2. 아이디 비밀번호 찾기 버튼 추가(Link Tag)
    
    * 아이디/비밀번호 찾기 페이지 추가
    
    아이디/비밀번호 찾기 페이지 추가하였습니다.
    각각 컴포넌트 화면으로 스위칭하게 처리할 예정입니다.
    
    * Update FindEmail.tsx
    
    아이디 찾기 화면 수정
    
    * Create MyPageCategoryList.tsx
    
    마이페이지 클릭 - 카테고리 목록 화면 만들었습니다.
    이 페이지에서 마이페이지수정 화면으로 넘어가는 것으로 기획되어 있습니다. 확인부탁드려용
    
    * Login-Header 생성
    
    Login-Header 생성하였습니다.
    메인에서도 같은 header를 사용할지 몰라 Login 용 헤더로 지정하였습니다.
    추후 같은 헤더로 사용할 시 한개의 디렉터리 바깥으로 뺄 예정입니다.
    
    * 글로벌 스타일 커밋
    
    ```
    styled-componenets
    @type-styled-componenets
    ```
    추가하였습니다.
    
    src/Index.tsx 에 해당 내용 import 했습니다. 확인 부탁드립니다.
    
    * 전역 스타일 수정
    
    전역 스타일 수정하였습니다.
    모든 파일 기본 div 클래스는 container 로 설정 부탁드립니다.
    
    * 폰트 내용 추가
    * Update GlobalStyles.tsx
    글로벌 스타일 추가 수정하였습니다.
    	--border-color-1: #E9E7E2; /* input border-color */
    	--main-text-color-1: #121212; /* 메인 텍스트색*/
    	--button-bg-color-1: #3D3938; /*로그인버튼배경색*/
    
    * 로그인 페이지 수정
    * Update tsconfig.json
     - 기존의 path 관련해서 오류가 발생하여 해당 내용 삭제하였습니다.

---
+++ src/assets/GlobalStyles.tsx
@@ -0,0 +1,82 @@
+import { createGlobalStyle } from "styled-components";
+
+/**
+ * @설명 전역 스타일
+ * @작성자 김상훈
+ * @생성일자 2023.03.30.
+ * ---------------------------------------- 
+ *  수정일자      수정자      내용
+ * ---------------------------------------- (공백3회)
+ * 2023.03.30.   김상훈   container 전역 추가
+ */
+const GlobalStyle = createGlobalStyle`
+html, body, #root, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	font-family: 'NanumSquareNeo-Variable';
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	vertical-align: baseline;
+}
+
+input {
+  box-sizing : border-box;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+/* custom 내용입니다 */
+.container{
+	width: 100%;
+	max-width: 420px;
+	min-height: 100vh;
+	margin: 0 auto;
+	padding: 0 16px; /*디자인에서 지정한 고정 padding입니다*/
+	background-color: #FAF9F6;
+}
+:root {
+	--border-color-1: #E9E7E2; /* input border-color */
+	--main-text-color-1: #121212; /* 메인 텍스트색*/
+	--button-bg-color-1: #3D3938; /*로그인버튼배경색*/
+}
+html, body, #root {
+	max-height: 100vh;
+	background-color: #FAF9F6;
+
+}
+`;
+
+export default GlobalStyle;
---
+++ src/components/auth/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <h1 style={loginHeader}>Goming.</h1>
+  ) 
+}
+
+const loginHeader = {
+  width: '100%',
+  height: '60px',
+  fontSize: '24px',
+  fontWeight: 'bold',
+  color: '#121212',
+  verticalAlign: 'middle',
+  lineHeight: '60px',
+
+}
+export default Header
