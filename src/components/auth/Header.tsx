ad4a8030f242f86f0ad71626c88fea9545adc7d8
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-03-30 17:54:46 +0900

    로그인 페이지 수정
    로그인 페이지 UI 적용하였습니다.

--- src/components/auth/Header.tsx
+++ src/components/auth/Header.tsx
@@ -1,14 +1,17 @@
 const Header = () => {
   return (
-    <>
-      <h1 style={loginHeader}>Goming.</h1>
-    </>
+    <h1 style={loginHeader}>Goming.</h1>
   ) 
 }
 
 const loginHeader = {
   width: '100%',
   height: '60px',
-  fontSize: '24px'
+  fontSize: '24px',
+  fontWeight: 'bold',
+  color: '#121212',
+  verticalAlign: 'middle',
+  lineHeight: '60px',
+
 }
 export default Header
