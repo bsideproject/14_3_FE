a5f5c813379e9a6bc3997f6d792f46b74880f410
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-03-30 13:11:47 +0900

    Update FindEmail.tsx
    아이디 찾기 화면 수정

--- src/components/auth/lost-info/FindEmail.tsx
+++ src/components/auth/lost-info/FindEmail.tsx
@@ -4,19 +4,31 @@ const FindEmail = () => {
   const [findName, setFindName] = useState<string>('')
   const [findNameVerify, setFindNameVerify] = useState<boolean>(false)
 
-  const handleFindName = ():void => { //keyup 
-    if (findName.length < 4) {
-
+  const handleFindName = ():void => { //keyup : 유효성
+    if (findNameVerify === false) {
+      if (findName.length > 4) {
+        setFindNameVerify(true)
+      } 
+    } else if (findNameVerify === true) {
+      if (findName.length < 4) {
+        setFindNameVerify(false)
+      } 
     }
   }
   const submitForm = (e:any):void => { //submit
     e.prevent.default()
+    if (findName.length < 1) {
+      alert('닉네임 혹은 이름을 작성해주세요')
+      return ;
+    } else {
+      //axios~
+    }
   }
   return (
     <>
       <div>
         <h4>아이디를 잊어버리셨나요?</h4>
-        <p>저희 고밍의 아이디는 이메일로 되어있어요</p>
+        <p>고밍의 아이디는 이메일로 되어있어요</p>
         <div>
           <form action="submitForm">
             <input type="text" value={findName} onKeyUp={handleFindName} />
