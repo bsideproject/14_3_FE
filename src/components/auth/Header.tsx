4877513d8502b026623e49e2fda1e0036f7dd4b0
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-03-30 14:34:35 +0900

    Login-Header 생성
    Login-Header 생성하였습니다.
    메인에서도 같은 header를 사용할지 몰라 Login 용 헤더로 지정하였습니다.
    추후 같은 헤더로 사용할 시 한개의 디렉터리 바깥으로 뺄 예정입니다.

---
+++ src/components/auth/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <h1 style={loginHeader}>Goming.</h1>
+    </>
+  ) 
+}
+
+const loginHeader = {
+  width: '100%',
+  height: '60px',
+  fontSize: '24px'
+}
+export default Header
