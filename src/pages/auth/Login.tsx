0d84e9c1992267cddae3b70840466651869d989d
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-04-05 16:41:42 +0900

    디자인 반영

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -101,45 +101,41 @@ const Login:React.FC = () =>{
 
   return (
     <>
-      <div className="container">
+      <Header title="로그인" />
+      <h1 className='startGomingText text-color'>로그인하고<br />오늘의 Goming을 시작해보세요!</h1>
+      <form onSubmit={loginAttempt}>
+        <div className='inputArea'>
+          <label htmlFor="email" className='login-label-text text-color'>이메일</label>
+          <input type="email" className='input-style'  placeholder='이메일 주소를 입력해주세요' id="email" value={email} onBlur={handleEmailBlur} onChange={handleEmailValue} maxLength={30} /><br />
+        </div>
+        <div className='checkbox-area'>
+          <input type="checkbox" className='save-id-check-box' name="rememberme" id="rememberId" checked={rememberEmail} onChange={handleRememberEmail} />
+          <label htmlFor="rememberId" className='save-id-label'>이메일 주소 기억하기</label>
+        </div>
         <div>
-        <Header title="로그인" />
-        <h1 className='startGomingText text-color'>로그인하고<br />오늘의 Goming을 시작해보세요!</h1>
-        <form onSubmit={loginAttempt}>
-          <div className='inputArea'>
-            <label htmlFor="email" className='login-label-text text-color'>이메일</label>
-            <input type="email" className='input-style'  placeholder='이메일 주소를 입력해주세요' id="email" value={email} onBlur={handleEmailBlur} onChange={handleEmailValue} maxLength={30} /><br />
-          </div>
-          <div className='checkbox-area'>
-            <input type="checkbox" className='save-id-check-box' name="rememberme" id="rememberId" checked={rememberEmail} onChange={handleRememberEmail} />
-            <label htmlFor="rememberId" className='save-id-label'>이메일 주소 기억하기</label>
-          </div>
-          <div>
-            <label htmlFor="password" className='login-label-text'>비밀번호</label>
-            <input type="password" className='input-style' placeholder='비밀번호를 입력해주세요' id="password" 
-              autoComplete='true'
-              value={password} 
-              onChange={handlePasswordValue} 
-              onKeyUp={handlePasswordValue} 
-              maxLength={30} />
-          </div>
-          <div className='login-btn-area'>
-            {
-              invalid 
-                ? <p className='blank-height-12'></p> 
-                : <p style={{color: '#EA4343', fontSize: '12px'}}>*이메일 혹은 비밀번호가 일치하지 않습니다.</p>
-            }
-            <button type="submit" className='login-btn'>로그인하기</button><br/>
-            {/* <button disabled type="button" className='login-btn' onClick={kakaoLogin}>Kakao Login Btn</button><br /> */}
-          </div>
-          <div className='link-btn-area'>
-            <Link className='link-btn' to="/register" type="button">회원가입하러가기</Link>
-            <img src={RectangleDived} alt="|" width={1} height={12} className='dived-link-img'/>
-            <Link className='link-btn' to="/lost-info">비밀번호 찾기</Link>
-          </div>
-        </form>
-            </div>
-      </div>
+          <label htmlFor="password" className='login-label-text'>비밀번호</label>
+          <input type="password" className='input-style' placeholder='비밀번호를 입력해주세요' id="password" 
+            autoComplete='true'
+            value={password} 
+            onChange={handlePasswordValue} 
+            onKeyUp={handlePasswordValue} 
+            maxLength={30} />
+        </div>
+        <div className='login-btn-area'>
+          {
+            invalid 
+              ? <p className='blank-height-12'></p> 
+              : <p style={{color: '#EA4343', fontSize: '12px'}}>*이메일 혹은 비밀번호가 일치하지 않습니다.</p>
+          }
+          <button type="submit" className='login-btn'>로그인하기</button><br/>
+          {/* <button disabled type="button" className='login-btn' onClick={kakaoLogin}>Kakao Login Btn</button><br /> */}
+        </div>
+        <div className='link-btn-area'>
+          <Link className='link-btn' to="/register" type="button">회원가입하러가기</Link>
+          <img src={RectangleDived} alt="|" width={1} height={12} className='dived-link-img'/>
+          <Link className='link-btn' to="/lost-info">비밀번호 찾기</Link>
+        </div>
+      </form>
     </>
   )
 }
--- src/pages/auth/LostInfo.tsx
+++ src/pages/auth/LostInfo.tsx
@@ -53,33 +53,34 @@ const LostInfo = () => {
   return (
     <>
       <div className="container">
-        <Header title="비밀번호 찾기" />
-        <h1 className='startGomingText text-color'>비밀번호 찾기</h1>
-        <form onSubmit={submitEmail}>
-          <div className='inputArea'>
-            <label htmlFor="email" className='login-label-text text-color'>이메일</label>
-            <input type="email" autoComplete="true"  className='input-style'  placeholder='이메일 주소를 입력해주세요' id="email" value={email} onBlur={handleEmailBlur} onChange={handleEmailValue} maxLength={30} /><br />
-          </div>
-          <div className='login-btn-area'>
-            <button type="submit" className='btn reset-password-btn'>비밀번호 초기화하기</button>
-            <button type="button" className='btn goto-login-btn' onClick={()=>goToLogin()}>로그인 하러가기</button>
-          </div>
-          <div>
-            <p className="find-id-caption-text">
-              *비밀번호 초기화 버튼 클릭시 기존에 설정하신 비밀번호가 초기화되며, 로그인하실 수 있는 임시 비밀번호가 등록된 메일로 전송됩니다.
-            </p>
-            <p className="find-id-caption-text">
-              *임시 비밀번호가 확인되지 않을시 스팸메일함 혹은 포르모션함을 확인해보시고 다시 한번 '비밀번호 초기화' 버튼을 눌러보세요.
-            </p>
-          </div>
-        </form>
-      </div>
+        <div className="inside-container">
+          <Header title="비밀번호 찾기" />
+          <div style={{height:'32px'}}></div>
+          <form onSubmit={submitEmail}>
+            <div className='inputArea'>
+              <label htmlFor="email" className='login-label-text text-color'>이메일</label>
+              <input type="email" autoComplete="true"  className='input-style'  placeholder='이메일 주소를 입력해주세요' id="email" value={email} onBlur={handleEmailBlur} onChange={handleEmailValue} maxLength={30} /><br />
+            </div>
+            <div className='login-btn-area'>
+              <button type="submit" className='btn reset-password-btn'>비밀번호 초기화하기</button>
+              <button type="button" className='btn goto-login-btn' onClick={()=>goToLogin()}>로그인 하러가기</button>
+            </div>
+            <div>
+              <p className="find-id-caption-text">
+                *비밀번호 초기화 버튼 클릭시 기존에 설정하신 비밀번호가 초기화되며, 로그인하실 수 있는 임시 비밀번호가 등록된 메일로 전송됩니다.
+              </p>
+              <p className="find-id-caption-text">
+                *임시 비밀번호가 확인되지 않을시 스팸메일함 혹은 포르모션함을 확인해보시고 다시 한번 '비밀번호 초기화' 버튼을 눌러보세요.
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>   
       {
         toastAlert === true ? (
           <ToastPopup text={"이메일로 임시 비밀번호가 전송되었습니다!"} bgColor={"#4D99DE"} textColor={"#FFFFFF"} />
         ) : ''
       }
-      
     </>
   )
 }
