fb17a4ae270b52d3a650e85b1d6b5e599f601fa0
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2023-04-11 15:53:46 +0900

    원페이저 내용 추가

--- src/pages/onpager/OnePagerMain.tsx
+++ src/pages/onpager/OnePagerMain.tsx
@@ -1,3 +1,6 @@
+
+import html2canvas from 'html2canvas';
+import canvasToImage from 'canvas-to-image'
 import Masonry from 'react-masonry-css'
 import 'assets/pages/onepager/onepagermain.css'
 import DefaultHeader from 'components/auth/DefaultHeader'
@@ -14,15 +17,46 @@ const testData = [
   {index: 10, content: 'contentsssssssssssssssssssssddddddddddddddsssssss'},
   {index: 11, content: 'contentssssssss'},
   {index: 12, content: 'contessssssssssssssssssssssssssntssssssss'},
+  {index: 13, content: 'contentssssgfggggggggggggggggggggggggggggggggggggggggggggggssss'},
+  {index: 14, content: 'contessssssssssssssssssssssssssntssssssss'},
+  {index: 15, content: 'contentssssssss'},
+  {index: 16, content: 'contessssssssssssssssssssssssssntssssssss'},
+  {index: 17, content: 'contentsssssssssssssssssssss'},
+  {index: 18, content: 'contensssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstssssssss'},
+  {index: 19, content: 'contentssssssss'},
+  {index: 20, content: 'contsssssssssssssssssentssssssss'},
+  {index: 21, content: 'contentssssssss'},
+  {index: 22, content: 'contentssssssss'},
+  {index: 23, content: 'contentssssssss'},
 ]
+const options = {
+  name: 'Goming-Onpager', // default image
+  type: 'png',         // default png, accepted values jpg or png
+  quality: 1         // default 1, can select any value from 0 to 1 range
+}
 const OnePagerMain = () => {
+  const onepager = () => {
+    const wrapper = document.querySelector('.onepager-download') as HTMLElement;
+    wrapper.style.width = "1920px"
+    html2canvas(wrapper).then(function(canvas) {
+      // document.body.appendChild(canvas);
+
+      canvasToImage(canvas, options);
+
+    });
+
+   
+  }
   return (
     <>
       <DefaultHeader />
       <h1 style={{marginTop: '20px'}}>Onepager</h1>
+      <div>
+        <button onClick={onepager}>원페이저다운로드</button>
+      </div>
       <div className='onepager-wrap'>
         <Masonry
-          breakpointCols={3}                          //컬럼수
+          breakpointCols={4}                          //컬럼수
           className="my-masonry-grid"
           columnClassName="my-masonry-grid_column">
           {
@@ -36,6 +70,25 @@ const OnePagerMain = () => {
         </Masonry>
 
       </div>
+
+      <div className='onepager-wrap onepager-download' style={{display: 'none'}}>
+        <Masonry
+          width={1920}
+          breakpointCols={12}                          //컬럼수
+          className="my-masonry-grid-download"
+          columnClassName="my-masonry-grid_column-download">
+          {
+            testData.map(item => (
+              <div key={item.index}>
+                <p className='word-wrap-break-word'>{item.index}</p>
+                <p className='word-wrap-break-word'>{item.content}</p>
+              </div>
+            ))
+          }
+        </Masonry>
+
+      </div>
+     
     </>
   )
 }
