172c041c6d445a2e4b203686d06d03bfb0cde05d
DoubleD <53948297+Hoon-Hub@users.noreply.github.com>
2023-04-20 23:32:13 +0900

    Shkim answer&edit (#32)
    * 기획, 수정 반영
    * 팝업 공통 수정

--- src/components/ConfirmPopup.tsx
+++ src/components/ConfirmPopup.tsx
@@ -3,11 +3,18 @@ import 'assets/components/alertTextPopup.css'
  * @설명 확인 팝업 창
  * @작성자 김상훈
  * @생성일자 2023.04.06.
+ * @param {string} strongText 강조 text 내용
  * @param {string} text text 내용
+ * @param {string} confirmText 예 
+ * @param {string} cancelText 아니오
  * @param {Function} callbackFunction 확인 콜백 함수
  * @param {Function} closeCallbackFuntion 아니오 콜백 함수
+ * @param {boolean} isFlex flex style 지정 여부
  */
-const ConfirmPopup = ({text, callbackFunction, closeCallbackFuntion}: CONFIRM_POPUP) => {
+const ConfirmPopup = (
+  {strongText, text, confirmText='네', cancelText='아니오', callbackFunction, closeCallbackFuntion, isFlex=true}
+  : CONFIRM_POPUP
+  ) => {
   // '네'를 눌렀을 경우
   const confirmCheck = () => {
     callbackFunction()
@@ -23,12 +30,20 @@ const ConfirmPopup = ({text, callbackFunction, closeCallbackFuntion}: CONFIRM_PO
       <div className='alert-bg'> 
         <div className='padding-x-24 relative'>
           <div className='confirm-modal'>
+            {/* 강조 텍스트 */}
+            <div className='headline3' style={{textAlign:'center', marginBottom:'12px'}}>
+              {strongText}
+            </div>
+            {/* 일반 텍스트 */}
             <div className='modal-text-area'>
               <p>{text}</p>
             </div>
-            <div className='confirm-btn-area'>
-              <button className='modal-btn-confirm' type="button" onClick={confirmCheck}>네</button>
-              <button className='modal-btn-confirm-cancel' type="button" onClick={closeConfirmPopup}>아니오</button>
+            {/* isFlex 전달여부에 따라 flex-direction 처리 */}
+            <div className={'confirm-btn-area ' + (isFlex && ('btns-isflex'))}>
+              {/* 취소 */}
+              <button className='modal-btn-confirm-cancel body3-bold' type="button" onClick={closeConfirmPopup}>{cancelText}</button>
+              {/* 확인 */}
+              <button className='modal-btn-confirm body3-bold' type="button" onClick={confirmCheck}>{confirmText}</button>
             </div>
           </div>
         </div>
@@ -37,9 +52,13 @@ const ConfirmPopup = ({text, callbackFunction, closeCallbackFuntion}: CONFIRM_PO
   )
 }
 type CONFIRM_POPUP = {
-  text: string
+  strongText?: string
+  text?: string
+  confirmText?: string
+  cancelText?: string
   callbackFunction: Function
   closeCallbackFuntion: Function
+  isFlex?: boolean
 }
 
 export default ConfirmPopup
