41c4f8dc8ad17651342c7766d4075ef3aab699c2
DoubleD <53948297+Hoon-Hub@users.noreply.github.com>
2023-05-12 21:31:12 +0900

    v1.1.0 반영 (#62)

--- src/components/mypage/Withdrawal3.tsx
+++ src/components/mypage/Withdrawal3.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import WithdrawalInformation from "./WithdrawalInformation"
+import useWithdrawalStates from "store/modules/Withdrawals"
 
 /**
  * @설명 회원탈퇴 세번째 페이지의 내용 컴포넌트
@@ -8,6 +10,20 @@ import WithdrawalInformation from "./WithdrawalInformation"
  * @param {Function} setStep useState의 setStep 함수 
  */
 const Withdrawal3 = ({step, setStep}: WITHDRAWAL) => {
+  const [withdrawalText, setWithdrawalText] = useState<string>('') //textarea 값
+  const {setWithdrawalState, withdrawal} = useWithdrawalStates() //회원탈퇴 관련 states
+  //탈퇴하기 버튼 클릭 이벤트
+  const withdrawalLastStep = async () => {
+    //store 에서 회원탈퇴 저장 및 api 호출
+    setWithdrawalState(withdrawalText)
+    const result = await withdrawal() //회원탈퇴 api 호출
+    if (result === false) {
+      setStep(++step)
+    } else {
+      //모든 로직 종료 후 step 값 변경
+       setStep(++step)
+    }
+  }
   return (
     <>
       <div className='withdrawal-content'>
@@ -26,15 +42,17 @@ const Withdrawal3 = ({step, setStep}: WITHDRAWAL) => {
           {/* textarea  */}
           <div style={{marginTop: '12px'}}>
             <textarea 
-              id="withdrawalTextarea"
+              id="withdrawalTextarea2"
+              value={withdrawalText}
+              onChange={(e) => setWithdrawalText(e.target.value)}
               className="withdrawal-textarea body3-regular" cols={30} rows={10} 
               placeholder="글자 수 제한없이, 고밍에게 하고 싶은 말을 자유롭게 남겨 주세요.&#13;&#10;좋았던 점, 개선되었으면 하는 점 등, 뭐든지 좋아요."
-              maxLength={1000} 
+              maxLength={1000}
               ></textarea>
           </div>
 
           <div className='withdrawal-button-area'>
-            <button className='withdrawal-button btn-p-xl body3-bold' type="button" onClick={() => setStep(++step)}>탈퇴하기</button>
+            <button className='withdrawal-button btn-p-xl body3-bold' type="button" onClick={() => withdrawalLastStep()}>탈퇴하기</button>
           </div>
           
         </div>
