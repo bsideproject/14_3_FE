e43457d3065d09bfd869b053e54daaf701e00ec3
DoubleD <53948297+Hoon-Hub@users.noreply.github.com>
2023-05-29 22:26:02 +0900

    Shkim 0528 (#74)

--- src/pages/main/MainContent.tsx
+++ src/pages/main/MainContent.tsx
@@ -4,7 +4,7 @@ import NowSelectionStep from "components/main/NowSelectionStep"
 import TodayDate from "components/main/TodayDate"
 import useCardState from "store/modules/CardState"
 import TodayAnswerCompleted from "components/main/TodayAnswerCompleted"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import useDefaultSets from "store/modules/Defaults"
 import Header from "components/auth/Header"
 import Footer from "components/Footer"
@@ -23,9 +23,8 @@ const MainContent = () => {
   const {
     todayCardSelectStep,   //금일 남은 답변 횟수
     getCardSelectStep,     //금일 남은 답변 횟수 가져오기 [1-3]
-    getFourSelectCards,    //4개의 카드 정보 가져오기
     todayCardSelectStatus, //금일 카드 뽑기 가능 여부
-    getOneCategory,        //카테고리 1개 조회
+    getCards,              //4개의 카드 정보 가져오기
   } = useCardState()       //카드 상태 관리 store
 
   useEffect(() => {
@@ -40,8 +39,7 @@ const MainContent = () => {
     if (todayCardSelectStep > 3) {      //3번 모두 답변했을 경우
       navigate('/answer/complete', {replace: true})  
     } else {
-      getOneCategory(userInfo.usrNo)    //카테고리 1개 조회
-      getFourSelectCards(userInfo.eml)  //4개의 카드 정보 가져오기
+      getCards(userInfo.eml)  //4개의 카드 정보 가져오기 or 단일 카드 정보 조회
     }
 
     return () => setIsNavigation(false)
@@ -68,7 +66,7 @@ const MainContent = () => {
             <div style={{margin: '0 16px'}}>
               <TodayDate />      {/* 금일자, 사용자정보 */}
               <NowSelectionStep nowStep={todayCardSelectStep} /> {/* 잔여 선택회수 */}
-              <SelectionCard />  {/* 카드 4개 출력 */}
+              <SelectionCard/>  {/* 카드 4개 출력 */}
             </div>
           </div>
           <Footer></Footer>
--- src/store/modules/Auth.ts
+++ src/store/modules/Auth.ts
@@ -1,6 +1,6 @@
 import { TYPE_USER_INFO } from "types/authTypes";
-import fetch from "utils/fetch";
-import { create } from "zustand";
+import { persist, PersistOptions } from "zustand/middleware";
+import { create, StateCreator } from "zustand";
 
 interface AUTH_STATE {
   userInfo: TYPE_USER_INFO; //사용자 정보
@@ -12,60 +12,69 @@ interface AUTH_STATE {
   logout: () => void; //로그아웃
 }
 
-const useAuthStore = create<AUTH_STATE>((set) => ({
-  userInfo: {
-    usrNo: null,
-    eml: "",
-    usrNm: null,
-    snsClsCd: null,
-    snsToken: null,
-    gndrClsCd: null,
-    brdt: null,
-    joinDtm: null,
-    lastLgnDtm: null,
-    updateDtm: null,
-    whdwlDtm: null,
-  },
-  isLogin: false,
-  isInfoChange: false,
-  updateLoginStatus: (
-    newLoginState: boolean,
-    userInfo: TYPE_USER_INFO
-  ): void => {
-    console.log("updateLoginStatus start");
-    console.log("newLoginState", newLoginState);
-    console.log("userInfo", userInfo);
-    
-    set({ isLogin: newLoginState });
-    set({ userInfo: userInfo });
-    console.log("updateLoginStatus end");
-  },
-  updateInfoChangeStatus: (newInfoChange: boolean): void => {
-    set({ isInfoChange: newInfoChange });
-  },
+const useAuthStore = create<AUTH_STATE>(
+  (persist as pillListPersist)((set) => ({
+    userInfo: {
+      usrNo: null,
+      eml: "",
+      usrNm: null,
+      snsClsCd: null,
+      snsToken: null,
+      gndrClsCd: null,
+      brdt: null,
+      joinDtm: null,
+      lastLgnDtm: null,
+      updateDtm: null,
+      whdwlDtm: null,
+    },
+    isLogin: false,
+    isInfoChange: false,
+    updateLoginStatus: (
+      newLoginState: boolean,
+      userInfo: TYPE_USER_INFO
+    ): void => {
+      console.log("updateLoginStatus start");
+      console.log("newLoginState", newLoginState);
+      console.log("userInfo", userInfo);
+      
+      set({ isLogin: newLoginState });
+      set({ userInfo: userInfo });
+      console.log("updateLoginStatus end");
+    },
+    updateInfoChangeStatus: (newInfoChange: boolean): void => {
+      set({ isInfoChange: newInfoChange });
+    },
 
-  /**
-   * @desc 회원탈퇴
-   * @param email
-   * @returns {boolean}
-   */
-  withdrawalUser: (email: string|null): boolean => {
-    const param = { eml: email };
-    //const result = fetch('/api/widthdrawalUser', param)   //db
-    set({ isLogin: false }); //islogin
-    set({ userInfo: initialUserState }); //reset
-    return true;
-  },
+    /**
+     * @desc 회원탈퇴
+     * @param email
+     * @returns {boolean}
+     */
+    withdrawalUser: (email: string|null): boolean => {
+      const param = { eml: email };
+      //const result = fetch('/api/widthdrawalUser', param)   //db
+      set({ isLogin: false }); //islogin
+      set({ userInfo: initialUserState }); //reset
+      return true;
+    },
 
-  /**
-   * @desc 로그아웃
-   * @returns N/A
-   */
-  logout: (): void => {
-    set({ isLogin: false }); //islogin
-    set({ userInfo: initialUserState }); //reset
-  }
-}));
+    /**
+     * @desc 로그아웃
+     * @returns N/A
+     */
+    logout: (): void => {
+      set({ isLogin: false }); //islogin
+      set({ userInfo: initialUserState }); //reset
+    }
+  }),
+
+
+  /*******************************************
+   * api 종료
+   ********************************************/
+  {name: "authStateStore",}
+  )
+);  
 
 const initialUserState = {
   usrNo: null,
@@ -80,5 +89,11 @@ const initialUserState = {
   updateDtm: null,
   whdwlDtm: null,
 };
+
+//persist
+type pillListPersist = (
+  config: StateCreator<AUTH_STATE>,
+  options: PersistOptions<AUTH_STATE>
+) => StateCreator<AUTH_STATE>;
 
 export default useAuthStore;
