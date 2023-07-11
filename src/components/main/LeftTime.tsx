4fd40c9a83612422d6ae85718f3e8a685db19246
DoubleD <53948297+Hoon-Hub@users.noreply.github.com>
2023-07-11 21:52:30 +0900

    답변 건너뛰기 및 일부 소스 수정 (#92)

--- src/components/main/LeftTime.tsx
+++ src/components/main/LeftTime.tsx
@@ -1,4 +1,9 @@
+import useAuthStore from "store/modules/Auth"
+
 const LeftTime = () => {
+  //사용자 이름 store 에서 가져오기
+  const { userInfo } = useAuthStore()
+
   //금일날짜
   const today = new Date()
   const todayDate = today.getMonth() + '월 ' + today.getDate() + '일 '
@@ -6,7 +11,7 @@ const LeftTime = () => {
     <>
       <p>{todayDate}</p>
       <h1>
-        커피중독자님(이름),<br />
+        {userInfo.usrNm ? userInfo.usrNm : "커피중독자"}님(이름),<br />
         오늘의 회고 질문을 선택해보세요!
       </h1>
     </>
