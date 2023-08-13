824f4e454cbe06cffcc3669cefab6aef01e00536
wpdbsx <wpdbsx@naver.com>
2023-08-14 06:00:35 +0900

    test

--- src/pages/onpager/OnePagerMain.tsx
+++ src/pages/onpager/OnePagerMain.tsx
@@ -76,7 +76,7 @@ const OnePagerMain = () => {
     const canvas = await html2canvas(wrapper, {
       allowTaint: true,
       useCORS: true,
-      scale: 0.833,
+      scale: 0.84,
     }); //scale 2 옵션으로 출력   => 1920px
     // alert(canvas.getContext);
     alert(canvas.height);
